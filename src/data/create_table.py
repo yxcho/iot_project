95f68dc6f4a054b79cec9738d288e18a1571a2ea
liuling950201 <1528784158@qq.com>
2022-10-12 22:44:04 +0800

    remove database credentials

--- src/data/create_table.py
+++ src/data/create_table.py
@@ -6,11 +6,11 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 
-DB_USER = "postgres"
-DB_HOST="localhost"
-DB_NAME="IoT"
-DB_PASSW=""
-DB_PORT=5432
+DB_USER = os.getenv("DB_USER")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_PASSW = os.getenv("DB_PASSW")
+DB_PORT = os.getenv("DB_PORT")
 
 SQLALCHEMY_DATABASE_URI =  f'postgresql://{DB_USER}:{DB_PASSW}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 engine = db.create_engine(SQLALCHEMY_DATABASE_URI)
